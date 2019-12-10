10ea2c81a0ccba91debe8c42d2463950dceb05a8
Mart Oruaas <fazz@pobox.com>
2019-12-10 07:34:48 +0200

    Day9

--- day09p1.py
+++ day09p1.py
@@ -1,19 +1,44 @@
 
 from copy import deepcopy
 
-def value(memory, position, mode):
+def getmem(memory, position):
+    if position > len(memory)-1:
+        memory.extend([0] * (1 + position - len(memory)))
+
+    if position < 0:
+        raise "Subzero address on read"
+
+    return memory[position]
+
+def setmem(memory, position, value):
+    getmem(memory, position)
+
+    if position < 0:
+        raise "Subzero address on write"
+
+    memory[position] = value
+    return memory
+
+# 0 - position mode
+# 1 - immediate mode
+# 2 - relative mode
+def value(memory, position, mode, rel):
     if mode == 0:
-        return memory[memory[position]]
+        return getmem(memory, getmem(memory, position))
     if mode == 1:
         return memory[position]
+    if mode == 2:
+        return getmem(memory, getmem(memory, position) + rel)
 
-def position(memory, position, mode):
+def position(memory, position, mode, rel):
     if mode == 0:
-        return memory[position]
+        return getmem(memory, position)
     if mode == 1:
         raise "Unspecified?"
+    if mode == 2:
+        return getmem(memory, position) + rel
 
-def cont(memory, pc, input, output):
+def cont(memory, pc, rel, input, output):
     while memory[pc] != 99:
         opcode = memory[pc] % 100
         p1mode = (memory[pc] // 100 ) % 10
@@ -22,61 +47,66 @@ def cont(memory, pc, input, output):
 
         if opcode == 1:
             pcinc = 4
-            op1 = value(memory, pc+1, p1mode)
-            op2 = value(memory, pc+2, p2mode)
-            resultpos = position(memory, pc+3, p3mode)
-            memory[resultpos] = op1+op2
+            op1 = value(memory, pc+1, p1mode, rel)
+            op2 = value(memory, pc+2, p2mode, rel)
+            resultpos = position(memory, pc+3, p3mode, rel)
+            setmem(memory, resultpos, op1+op2)
             
         elif opcode == 2:
             pcinc = 4
-            op1 = value(memory, pc+1, p1mode)
-            op2 = value(memory, pc+2, p2mode)
-            resultpos = position(memory, pc+3, p3mode)
-            memory[resultpos] = op1*op2
+            op1 = value(memory, pc+1, p1mode, rel)
+            op2 = value(memory, pc+2, p2mode, rel)
+            resultpos = position(memory, pc+3, p3mode, rel)
+            setmem(memory, resultpos, op1*op2)
 
         elif opcode == 3:
             pcinc = 2
-            resultpos = position(memory, pc+1, p1mode)
+            resultpos = position(memory, pc+1, p1mode, rel)
             head, *tail = input
-            memory[resultpos] = head
+            setmem(memory, resultpos, head)
             input = tail
 
         elif opcode == 4:
             pcinc = 2
-            op1 = value(memory, pc+1, p1mode)
+            op1 = value(memory, pc+1, p1mode, rel)
             output.append(op1)
             # When output, return
-            return (input, output, memory, pc+pcinc)
+            return (input, output, memory, pc+pcinc, rel)
 
         elif opcode == 5:
             pcinc = 3
-            op1 = value(memory, pc+1, p1mode)
-            op2 = value(memory, pc+2, p2mode)
+            op1 = value(memory, pc+1, p1mode, rel)
+            op2 = value(memory, pc+2, p2mode, rel)
             if op1 != 0:
                 pc = op2
                 pcinc = 0
 
         elif opcode == 6:
             pcinc = 3
-            op1 = value(memory, pc+1, p1mode)
-            op2 = value(memory, pc+2, p2mode)
+            op1 = value(memory, pc+1, p1mode, rel)
+            op2 = value(memory, pc+2, p2mode, rel)
             if op1 == 0:
                 pc = op2
                 pcinc = 0
 
         elif opcode == 7:
             pcinc = 4
-            op1 = value(memory, pc+1, p1mode)
-            op2 = value(memory, pc+2, p2mode)
-            resultpos = position(memory, pc+3, p3mode)
+            op1 = value(memory, pc+1, p1mode, rel)
+            op2 = value(memory, pc+2, p2mode, rel)
+            resultpos = position(memory, pc+3, p3mode, rel)
             memory[resultpos] = 1 if op1 < op2 else 0
 
         elif opcode == 8:
             pcinc = 4
-            op1 = value(memory, pc+1, p1mode)
-            op2 = value(memory, pc+2, p2mode)
-            resultpos = position(memory, pc+3, p3mode)
-            memory[resultpos] = 1 if op1 == op2 else 0
+            op1 = value(memory, pc+1, p1mode, rel)
+            op2 = value(memory, pc+2, p2mode, rel)
+            resultpos = position(memory, pc+3, p3mode, rel)
+            setmem(memory, resultpos, 1 if op1 == op2 else 0)
+
+        elif opcode == 9:
+            pcinc = 2
+            op1 = value(memory, pc+1, p1mode, rel)
+            rel += op1
 
         else:
             raise "Unknown opcode"
@@ -86,63 +116,26 @@ def cont(memory, pc, input, output):
     # Output end marker
     output.append('STOP')
 
-    return (input, output, memory, pc)
+    return (input, output, memory, pc, rel)
 
 # First run from image
 def execute(image, input, output):
     memory = image.copy()
     pc = 0
-    return cont(memory, pc, input, output)
+    return cont(memory, pc, 0, input, output)
 
-inputprogram='3,8,1001,8,10,8,105,1,0,0,21,38,63,88,97,118,199,280,361,442,99999,3,9,1002,9,3,9,101,2,9,9,1002,9,4,9,4,9,99,3,9,101,3,9,9,102,5,9,9,101,3,9,9,1002,9,3,9,101,3,9,9,4,9,99,3,9,1002,9,2,9,1001,9,3,9,102,3,9,9,101,2,9,9,1002,9,4,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,102,4,9,9,101,5,9,9,102,2,9,9,101,5,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,99'
-
-#inputprogram='3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
-
-#inputprogram='3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'
+inputprogram='1102,34463338,34463338,63,1007,63,34463338,63,1005,63,53,1102,3,1,1000,109,988,209,12,9,1000,209,6,209,3,203,0,1008,1000,1,63,1005,63,65,1008,1000,2,63,1005,63,902,1008,1000,0,63,1005,63,58,4,25,104,0,99,4,0,104,0,99,4,17,104,0,99,0,0,1101,309,0,1024,1101,0,24,1002,1102,388,1,1029,1102,1,21,1019,1101,0,33,1015,1102,1,304,1025,1101,344,0,1027,1101,25,0,1003,1102,1,1,1021,1101,29,0,1012,1101,0,23,1005,1102,1,32,1007,1102,38,1,1000,1101,30,0,1016,1102,1,347,1026,1101,0,26,1010,1101,0,39,1004,1102,1,36,1011,1101,0,393,1028,1101,0,37,1013,1101,0,35,1008,1101,34,0,1001,1101,0,495,1022,1102,1,28,1018,1101,0,0,1020,1102,1,22,1006,1101,488,0,1023,1102,31,1,1009,1102,1,20,1017,1101,0,27,1014,109,10,21102,40,1,4,1008,1014,37,63,1005,63,205,1001,64,1,64,1106,0,207,4,187,1002,64,2,64,109,-18,1207,8,37,63,1005,63,227,1001,64,1,64,1106,0,229,4,213,1002,64,2,64,109,17,1207,-7,25,63,1005,63,247,4,235,1106,0,251,1001,64,1,64,1002,64,2,64,109,-8,1202,6,1,63,1008,63,29,63,1005,63,275,1001,64,1,64,1106,0,277,4,257,1002,64,2,64,109,25,1205,-6,293,1001,64,1,64,1105,1,295,4,283,1002,64,2,64,109,-4,2105,1,2,4,301,1106,0,313,1001,64,1,64,1002,64,2,64,109,-9,1208,-4,31,63,1005,63,335,4,319,1001,64,1,64,1105,1,335,1002,64,2,64,109,16,2106,0,-2,1106,0,353,4,341,1001,64,1,64,1002,64,2,64,109,-13,2102,1,-8,63,1008,63,38,63,1005,63,373,1105,1,379,4,359,1001,64,1,64,1002,64,2,64,109,9,2106,0,3,4,385,1105,1,397,1001,64,1,64,1002,64,2,64,109,-11,21107,41,42,0,1005,1014,415,4,403,1106,0,419,1001,64,1,64,1002,64,2,64,109,14,1206,-7,431,1106,0,437,4,425,1001,64,1,64,1002,64,2,64,109,-23,2107,37,-5,63,1005,63,455,4,443,1105,1,459,1001,64,1,64,1002,64,2,64,109,10,21107,42,41,-2,1005,1013,475,1105,1,481,4,465,1001,64,1,64,1002,64,2,64,2105,1,8,1001,64,1,64,1106,0,497,4,485,1002,64,2,64,109,-6,21108,43,41,8,1005,1017,517,1001,64,1,64,1106,0,519,4,503,1002,64,2,64,109,5,2101,0,-9,63,1008,63,23,63,1005,63,541,4,525,1106,0,545,1001,64,1,64,1002,64,2,64,109,-13,1201,5,0,63,1008,63,20,63,1005,63,565,1105,1,571,4,551,1001,64,1,64,1002,64,2,64,109,16,1205,4,589,4,577,1001,64,1,64,1106,0,589,1002,64,2,64,109,-16,1202,4,1,63,1008,63,23,63,1005,63,615,4,595,1001,64,1,64,1106,0,615,1002,64,2,64,109,1,2101,0,6,63,1008,63,33,63,1005,63,639,1001,64,1,64,1105,1,641,4,621,1002,64,2,64,109,8,21101,44,0,8,1008,1018,44,63,1005,63,667,4,647,1001,64,1,64,1105,1,667,1002,64,2,64,109,-7,1201,1,0,63,1008,63,39,63,1005,63,689,4,673,1106,0,693,1001,64,1,64,1002,64,2,64,109,7,2102,1,-8,63,1008,63,24,63,1005,63,715,4,699,1105,1,719,1001,64,1,64,1002,64,2,64,109,5,2108,34,-7,63,1005,63,739,1001,64,1,64,1105,1,741,4,725,1002,64,2,64,109,-22,2108,25,10,63,1005,63,763,4,747,1001,64,1,64,1106,0,763,1002,64,2,64,109,31,1206,-4,781,4,769,1001,64,1,64,1105,1,781,1002,64,2,64,109,-10,21101,45,0,5,1008,1019,47,63,1005,63,805,1001,64,1,64,1105,1,807,4,787,1002,64,2,64,109,2,21108,46,46,-3,1005,1013,825,4,813,1106,0,829,1001,64,1,64,1002,64,2,64,109,-22,2107,40,10,63,1005,63,845,1105,1,851,4,835,1001,64,1,64,1002,64,2,64,109,17,1208,-7,36,63,1005,63,871,1001,64,1,64,1105,1,873,4,857,1002,64,2,64,109,16,21102,47,1,-9,1008,1018,47,63,1005,63,899,4,879,1001,64,1,64,1106,0,899,4,64,99,21102,1,27,1,21101,0,913,0,1105,1,920,21201,1,39657,1,204,1,99,109,3,1207,-2,3,63,1005,63,962,21201,-2,-1,1,21102,1,940,0,1105,1,920,21201,1,0,-1,21201,-2,-3,1,21101,955,0,0,1105,1,920,22201,1,-1,-2,1106,0,966,21202,-2,1,-2,109,-3,2105,1,0'
 
 origmemory = [int(x) for x in inputprogram.split(',')]
 
-def simulate(stages, currentmax):
-    stage = 0
-    # 1st stage gets 0 for starters
-    stages[stage][2].append(0)
-    while True:
-        # stages: memory, PC, input, output
+input = [2]
+output = []
 
-        output = []
+(input, output, memory, pc, rel) = execute(origmemory, input, output)
 
-        (input, output, memory, pc) = cont(stages[stage][0], stages[stage][1], stages[stage][2], stages[stage][3])
+while output[-1] != 'STOP':
+    (input, output, memory, pc, rel) = cont(memory, pc, rel, input, output)
 
-        stages[stage] = (memory, pc, input, output)
+print(output)
 
-        noout = False
-
-        if output[-1] == 'STOP':
-            if stage == 4:
-                return max(output[-2], currentmax)
-            else:
-                #raise "Simple STOP out of place?"
-                noout = True
-
-        stage = (stage + 1) % 5
-
-        # Push output forward
-        if not noout:
-            stages[stage][2].append(output[-1])
-
-
-def permutation(stages, stage, currentmax, options):
-    if not len(options):
-        return simulate(stages, currentmax)
-
-    for phase in options:
-        cstages = deepcopy(stages)
-        # memory, PC, input, output
-        cstages[stage] = (origmemory.copy(), 0, [phase], [])
-        currentmax = permutation(cstages, stage + 1, currentmax, options.difference({phase}))
-
-    return currentmax
-
-    
-print(permutation({}, 0, 0, set(range(5,10))))
 
