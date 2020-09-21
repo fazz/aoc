fac5544cc42c8dea2b5563fa4d490ef260329906
Mart Oruaas <fazz@pobox.com>
2020-09-21 13:08:11 +0300

    AoC 2015: day24

--- aoc2015/days/day24/day24.go
+++ aoc2015/days/day24/day24.go
@@ -16,9 +16,9 @@ import (
 
 type Solution struct {
 	Included  []int `hash:"set"`
-	SpaceLeft int
-	QE        int
-	Leftover  []int `hash:"set"`
+	spaceLeft int
+	qE        int
+	leftOver  []int
 }
 
 func Exec() {
@@ -46,146 +46,43 @@ func Exec() {
 		values = append(values, int(v))
 	}
 
-	//values = []int{1, 2, 3, 4, 5, 7, 8, 9, 10, 11}
-
 	sort.Slice(values, func(i, j int) bool { return values[i] > values[j] })
 
-	var compartmentSize int
+	var compartmentSize1, compartmentSize2 int
 	{
 		s := 0
 		for _, v := range values {
 			s += v
 		}
-		compartmentSize = s / 3
+		compartmentSize1 = s / 3
+		compartmentSize2 = s / 4
 	}
 
-	/*
-		// State storage
-		states := make(map[uint64]Solution)
+	c1 := make(chan int)
+	c2 := make(chan int)
 
-		// maps leftover size in comp. 1 to solution set
-		compSolutions := make(map[int]mapset.Set)
-
-		put := func(spaceLeft int, s Solution) {
-			hash, err := hashstructure.Hash(s, nil)
-			if err != nil {
-				panic("WTF")
-			}
-			states[hash] = s
-			if _, ok := compSolutions[spaceLeft]; !ok {
-				compSolutions[spaceLeft] = mapset.NewSet()
-			}
-			compSolutions[spaceLeft].Add(hash)
-			//fmt.Println("put", spaceLeft, s, hash)
-		}
-
-		// Initial state
-		initial := Solution{Included: []int{}, QE: 0, Leftover: values}
-
-		put(compartmentSize, initial)
-
-		for spaceLeft := compartmentSize; spaceLeft >= 0; spaceLeft-- {
-			if _, ok := compSolutions[spaceLeft]; !ok {
-				continue
-			}
-			fmt.Println("Processing LO", spaceLeft)
-			for s := range compSolutions[spaceLeft].Iterator().C {
-				//fmt.Println("s", s)
-				sh, ok := s.(uint64)
-				if !ok {
-					panic("WTF")
-				}
-				solution := states[sh]
-
-				for idx, candidate := range solution.Leftover {
-					if spaceLeft-candidate >= 0 {
-
-						head := solution.Leftover[0:idx]
-						tail := solution.Leftover[idx+1:]
-
-						newLeftOver := append(append([]int{}, head...), tail...)
-						newIncluded := append(append([]int{}, solution.Included...), candidate)
-
-						qe := 1
-						for _, s := range newIncluded {
-							qe *= s
-						}
-
-						newSpaceLeft := spaceLeft - candidate
-						put(newSpaceLeft, Solution{Included: newIncluded, QE: qe, Leftover: newLeftOver})
-					}
-				}
-			}
-		}
-		fmt.Println(compSolutions)
-
-		sortedSolutions := make([]Solution, 0)
-	*/
-
-	//currentSolutionLen := len(values)
-
-	QE1 := 0
-	var QE1Solution Solution
-
-	for s := range compute(compartmentSize, values) {
-		solution := *s
-		if testForHalving(compartmentSize, solution.Leftover) {
-			QE1 = solution.QE
-			QE1Solution = solution
+	go func(c chan int) {
+		var qE int
+		for s := range compute(compartmentSize1, values) {
+			solution := *s
+			qE = solution.qE
 			break
 		}
-	}
+		c <- qE
+	}(c1)
 
-	/*
-		for s := range compSolutions[0].Iterator().C {
-			sh, ok := s.(uint64)
-			if !ok {
-				panic("WTF")
-			}
-			solution := states[sh]
-
-			sortedSolutions = append(sortedSolutions, solution)
-
-			//		fmt.Println(solution)
+	go func(c chan int) {
+		var qE int
+		for s := range compute(compartmentSize2, values) {
+			solution := *s
+			qE = solution.qE
+			break
 		}
-		sort.Slice(sortedSolutions,
-			func(i, j int) bool {
-				if len(sortedSolutions[i].Included) < len(sortedSolutions[j].Included) {
-					return true
-				}
-				if len(sortedSolutions[i].Included) > len(sortedSolutions[j].Included) {
-					return false
-				}
-				if sortedSolutions[i].QE < sortedSolutions[j].QE {
-					return true
-				}
-				return false
-			},
-		)
+		c <- qE
+	}(c2)
 
-		QE1 := 0
-
-		for _, v := range sortedSolutions {
-			if testForHalving(compartmentSize, v.Leftover) {
-				fmt.Println(v)
-				QE1 = v.QE
-				break
-			}
-		}
-
-		//fmt.Println(compartmentSize)
-	*/
-	fmt.Println("Part 1:", QE1, QE1Solution)
-	//fmt.Println("Part 2:", calc(1, 0))
-}
-
-func testForHalving(compartmentSize int, values []int) bool {
-	found := false
-	for range compute(compartmentSize, values) {
-		found = true
-		break
-	}
-	return found
+	fmt.Println("Part 1:", <-c1)
+	fmt.Println("Part 2:", <-c2)
 }
 
 // A PriorityQueue implements heap.Interface and holds Items.
@@ -194,8 +91,18 @@ type PriorityQueue []*Solution
 func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq[i].SpaceLeft < pq[j].SpaceLeft
+
+	// First, order by solution len
+	if len(pq[i].Included) != len(pq[j].Included) {
+		return len(pq[i].Included) < len(pq[j].Included)
+	}
+	// Then by space left (0 takes precedence)
+	if pq[i].spaceLeft != pq[j].spaceLeft {
+		return pq[i].spaceLeft < pq[j].spaceLeft
+	}
+
+	// When equal (esp. for 0), order by qE
+	return pq[i].qE < pq[j].qE
 }
 
 func (pq *PriorityQueue) Push(x interface{}) {
@@ -218,67 +125,62 @@ func (pq PriorityQueue) Swap(i, j int) {
 
 func compute(compartmentSize int, values []int) chan *Solution {
 
-	outputChannel := make(chan *Solution)
+	outputChannel := make(chan *Solution, 1)
 
 	go func(c chan *Solution) {
 
 		pq := make(PriorityQueue, 0)
 		heap.Init(&pq)
 
-		// State storage
-		states := make(map[uint64]Solution)
+		compSolutions := mapset.NewSet()
 
-		// maps leftover size in a compartment to solution set
-		compSolutions := make(map[int]mapset.Set)
-
-		put := func(spaceLeft int, s Solution) bool {
+		put := func(s Solution) {
 			hash, err := hashstructure.Hash(s, nil)
 			if err != nil {
 				panic("WTF")
 			}
-			states[hash] = s
-			if _, ok := compSolutions[spaceLeft]; !ok {
-				compSolutions[spaceLeft] = mapset.NewSet()
-			}
-			added := compSolutions[spaceLeft].Add(hash)
+			added := compSolutions.Add(hash)
 
-			if added && spaceLeft > 0 {
+			if added {
 				pq.Push(&s)
 			}
-
-			return added
 		}
 
 		// Initial state
-		initial := Solution{Included: []int{}, SpaceLeft: compartmentSize, QE: 0, Leftover: values}
+		initial := Solution{Included: []int{}, spaceLeft: compartmentSize, qE: 0, leftOver: values}
 
-		put(compartmentSize, initial)
+		put(initial)
 
-		//for spaceLeft := compartmentSize; spaceLeft > 0; spaceLeft-- {
 		for pq.Len() > 0 {
 
 			solution := heap.Pop(&pq).(*Solution)
 
-			for idx, candidate := range solution.Leftover {
-				if solution.SpaceLeft-candidate >= 0 {
+			if solution.spaceLeft == 0 {
+				c <- solution
+			} else {
 
-					head := solution.Leftover[0:idx]
-					tail := solution.Leftover[idx+1:]
+				for idx, candidate := range solution.leftOver {
+					if solution.spaceLeft-candidate >= 0 {
 
-					newLeftOver := append(append([]int{}, head...), tail...)
-					newIncluded := append(append([]int{}, solution.Included...), candidate)
+						head := solution.leftOver[0:idx]
+						tail := solution.leftOver[idx+1:]
 
-					qe := 1
-					for _, s := range newIncluded {
-						qe *= s
-					}
+						newLeftOver := append(append([]int{}, head...), tail...)
+						newIncluded := append(append([]int{}, solution.Included...), candidate)
 
-					newSpaceLeft := solution.SpaceLeft - candidate
-					//fmt.Println("newSpaceLeft:", newSpaceLeft)
-					newSolution := Solution{Included: newIncluded, SpaceLeft: newSpaceLeft, QE: qe, Leftover: newLeftOver}
+						qe := 1
+						for _, s := range newIncluded {
+							qe *= s
+						}
 
-					if put(newSpaceLeft, newSolution) && newSpaceLeft == 0 {
-						c <- &newSolution
+						newSolution := Solution{
+							Included:  newIncluded,
+							spaceLeft: solution.spaceLeft - candidate,
+							qE:        qe,
+							leftOver:  newLeftOver,
+						}
+
+						put(newSolution)
 					}
 				}
 			}
