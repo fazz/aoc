cc4d055882293c6503b881c6c8be06a5b947c1c4
Mart Oruaas <fazz@pobox.com>
2023-01-02 12:17:26 +0200

    AoC 2022: day24

--- aoc2022/day24.py
+++ aoc2022/day24.py
@@ -1,71 +1,70 @@
-import datetime
-from copy import deepcopy
-from itertools import compress
-from functools import reduce, cmp_to_key
-import operator
-from collections import defaultdict
-import re
 
-from segments import intersect
-
-from queue import PriorityQueue
-
-start_time = datetime.datetime.now()
+from heapq import *
 
 lines = [x.rstrip(" \n\r") for x in open("input24.txt", "r")]
 
-(miny, maxy, mix, maxx) = (1, len(lines)-2, 1, len(lines[0])-2)
+(miny, maxy, minx, maxx) = (1, len(lines)-2, 1, len(lines[0])-2)
 
 start_pos = (lines[0].index('.'), miny-1)
 end_pos = (lines[-1].index('.'), maxy+1)
 
 bd = {'>': (1, 0), '<': (-1, 0), '^': (0, -1), 'v': (0, 1)}
 
-blizzards = set()
+blizzardminuteh = {0: {}}
+blizzardminutev = {0: {}}
 
 for li in range(len(lines)):
     for ci in range(len(lines[li])):
         if lines[li][ci] not in ('.', '#'):
-            blizzards.add((ci, li, *(bd[lines[li][ci]])))
-
-blizzardminute = {0: {(z[0], z[1]) for z in blizzards}}
+            if lines[li][ci] in ('<', '>'):
+                blizzardminuteh[0][(ci, li)] = bd[lines[li][ci]]
+            else:
+                blizzardminutev[0][(ci, li)] = bd[lines[li][ci]]
 
 def updateblizzard(minute):
-    p = max(blizzardminute)
+    p = max(blizzardminuteh)%maxx
 
-    for m in range(p+1, minute+1):
-        ns = set()
-        blizzardminute[m] = ns
-        for b in blizzards:
-            ns.add(((b[0]+b[2]*m-1)%maxx+1, (b[1]+b[3]*m-1)%maxy+1))
+    for m in range(p+1, minute%maxx+1):
+        ns = {}
+        blizzardminuteh[m] = ns
+        for b,d in blizzardminuteh[0].items():
+            ns[((b[0]+d[0]*m-1)%maxx+1, b[1])] = 1
+
+    p = max(blizzardminutev)%maxy
+
+    for m in range(p+1, minute%maxy+1):
+        ns = {}
+        blizzardminutev[m] = ns
+        for b,d in blizzardminutev[0].items():
+            ns[(b[0], (b[1]+d[1]*m-1)%maxy+1)] = 1
 
 def getdirections(minute, x, y):
-    if minute not in blizzardminute:
-        updateblizzard(minute)
-    bzrds = blizzardminute[minute]
+    bzrdsh = blizzardminuteh[minute%maxx]
+    bzrdsv = blizzardminutev[minute%maxy]
 
     ret = []
-    for (xd, yd) in ((0, 0), (-1, 0), (1, 0), (0, -1), (0, 1)):
-        (nx, ny) = (x+xd, y+yd)
-        if (nx >= 1 and nx <= maxx and ny >= 1 and ny <= maxy and (nx, ny) not in bzrds) or (nx, ny) == end_pos or (nx, ny) == start_pos:
+    for (nx, ny) in ((x, y), (-1+x, y), (1+x, y), (x, -1+y), (x, 1+y)):
+        if (nx >= 1 and nx <= maxx and ny >= 1 and ny <= maxy and (nx, ny) not in bzrdsh and (nx, ny) not in bzrdsv) or (nx, ny) == end_pos or (nx, ny) == start_pos:
             ret.append((minute, nx, ny))
 
     return ret
 
-q = PriorityQueue()
+updateblizzard(699)
 
-visited = set([(0, *start_pos)])
+q = []
 
-q.put((0, *start_pos))
+heappush(q, (0, 0, *start_pos))
 
 result1 = None
 result2 = None
 targets = [end_pos, start_pos, end_pos]
 
-while q.qsize() > 0:
-    curr = q.get()
+visited = set()
 
-    (minute, x, y) = curr
+while len(q) > 0:
+    curr = heappop(q)
+
+    (_, minute, x, y) = curr
 
     if (x,y) == targets[0]:
         targets.pop(0)
@@ -74,15 +73,18 @@ while q.qsize() > 0:
         if len(targets) == 0:
             result2 = minute
             break
-        q = PriorityQueue()
+        q = []
         visited = set(curr)
 
     free = getdirections(minute+1, x, y)
 
     for step in free:
+        predict = minute + 1 + abs(step[1]-targets[0][0]) + abs(step[2]-targets[0][1])
+
         if step not in visited:
             visited.add(step)
-            q.put(step)
+            heappush(q, (predict, *step))
 
-print("Part 1:", result1)
-print("Part 2:", result2)
+print("Part 1:", result1, 242)
+print("Part 2:", result2, 720)
+
